4a5b50fb5b5895f164a03fd3d3253346dceef8ec
dull <964545502@qq.com>
2021-06-10 10:27:46 +0800

    init

---
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/mapper/SysJobMapper.java
@@ -0,0 +1,13 @@
+package com.ruoyi.quartz.mapper;
+
+import com.ruoyi.common.core.page.BaseMapperPlus;
+import com.ruoyi.quartz.domain.SysJob;
+
+/**
+ * 调度任务信息 数据层
+ *
+ * @author ruoyi
+ */
+public interface SysJobMapper extends BaseMapperPlus<SysJob> {
+
+}
